1609edb4cd49359651f842a29f01eee21f8cd15e
landahab <86staves_quotas@icloud.com>
2025-08-02 18:41:48 +0100

    More unfucking of the great existing code

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <esp_now.h>
 
 #define MaxGearChangeThrottle 30
-int safeGearState = 0;
 
 // ----------------------------
 //   SETUP
@@ -15,20 +14,24 @@ int safeGearState = 0;
 // Declare classes
 // This uses the t-code object above
 
-#define GearServo_PIN 7
-#define GearServo_REVERSE 500
-#define GearServo_NEUTRAL 1000
-#define GearServo_FORWARD 1500
+#define GearServo_PIN 9
+#define GearServo_REVERSE 2000 
+#define GearServo_NEUTRAL 2300
+#define GearServo_FORWARD 2600
 
-#define ThrottleServo_PIN 8
-#define ThrottleServo_MIN 1000
-#define ThrottleServo_MAX 2000
+#define ThrottleServo_PIN 10
+#define ThrottleServo_MIN 2000
+#define ThrottleServo_MAX 2500
 #define ThrottleServo_ZERO (ThrottleServo_MIN + ThrottleServo_MAX) / 2
+
+#define THROTTLE_INPUT_MAX 84000
 
 #define BilgeRelay_PIN 6
 
-#define PWM_RESOLUTION_BITS 16
+#define PWM_RESOLUTION_BITS 12
 #define DS3225_Servo_Freq 330
+
+uint8_t newMACAddress[] = {0xb4,0x3a,0x45,0xb0,0xd4,0xa8};
 
 // Position variables
 int xLin;
@@ -40,17 +43,20 @@ float motorVoltage;
 uint64_t cycle = 0;
 uint64_t pktsReceived = 0;
 
-enum GEAR_STATE {
+enum FOREBOARD_GEAR_STATE {
   GEAR_STATE_REVERSE = -1,
   GEAR_STATE_NEUTRAL = 0,
-  GEAR_STATE_FORWARD = 1,
+  GEAR_STATE_FORWARD = 1
 };
+FOREBOARD_GEAR_STATE safeGearState = GEAR_STATE_NEUTRAL;
+
 
 class Servo
 {
 public:
   Servo() {}
-  Servo(int pin, int freq, int pwm_bits, int min, int max, int zero = -1)
+  
+  Servo(int pin, int freq, int pwm_bits, int in_min, int in_max, int out_min, int out_max)//, int zero = -1)
   {
     Serial.printf("Instantiating servo on pin %d\n", pin);
 
@@ -58,59 +64,43 @@ public:
     _freq = freq;
     _pwm_bits = pwm_bits; // resolution in bits
     _servo_int = 1000000 / freq;
-    _min = min;
-    _max = max;
-    if (zero < 0)
-    {
-      _zero = (min + max) / 2;
-    }
-    else
-    {
-      _zero = zero;
-    }
+    _in_min = in_min;
+    _in_max = in_max;
+    _out_min = out_min;
+    _out_max = out_max;
 
     // Initialize the servo pin
-    pinMode(_pin, OUTPUT);
-
     ledcAttach(_pin, _freq, _pwm_bits);
   }
 
-  void write(float ratio)
+  void write(int value)
   {
-    int writeValue;
-    // Ensure ratio is within bounds
-    if (ratio < 0.0f)
-    {
-      writeValue = _min;
+    int clamped;
+    if (value > _in_max) {
+      clamped = _in_max;
+    } else if (value < _in_min) {
+      clamped = _in_min;
+    } else {
+      clamped = value;
     }
-    else if (ratio > 1.0f)
-    {
-      writeValue = _max;
-    }
-    else
-    {
-      writeValue = static_cast<int>(ratio * (_max - _min) + _min);
-    }
-
-    ledcWrite(_pin, map(writeValue, 0, _servo_int, 0, 65535));
+    long out = map(clamped, _in_min, _in_max, _out_min, _out_max);
+    ledcWrite(_pin, out);
   }
 
 private:
-  void init(int pin, int freq, int pwm_bits, int min, int max, int zero = -1) {
-    
-  }
-
   int _pin;
   int _freq;
   int _pwm_bits;
-  int _servo_int = 0;
-  int _min;
-  int _max;
+  int _servo_int;
+  int _in_min;
+  int _in_max;
+  int _out_min;
+  int _out_max;
   int _zero;
 };
 
-Servo GearServo(GearServo_PIN, DS3225_Servo_Freq, PWM_RESOLUTION_BITS, GearServo_REVERSE, GearServo_FORWARD);
-Servo ThrottleServo(ThrottleServo_PIN, DS3225_Servo_Freq, PWM_RESOLUTION_BITS, ThrottleServo_MIN, ThrottleServo_MAX);
+Servo GearServo;
+Servo ThrottleServo;
 
 // Structure example to receive data
 // Must match the sender structure
@@ -119,7 +109,7 @@ typedef struct struct_message
   int id;
   int steer;    //-10000 to 10000
   int throttle; // 0 to 10000
-  GEAR_STATE gear;     //-1,0,1
+  FOREBOARD_GEAR_STATE gear;     //-1,0,1
   int bilge;    // 1,0
 } struct_message;
 
@@ -160,6 +150,8 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
 void setup()
 {
   Serial.begin(115200);
+  delay(2000);
+
   Serial.println("Setting up...");
 
   Serial.println("Initialising state");
@@ -169,13 +161,21 @@ void setup()
   foreBoard.bilge = 0;
 
   Serial.println("Setting pin modes");
-  pinMode(GearServo_PIN, OUTPUT);
-  pinMode(ThrottleServo_PIN, OUTPUT);
   pinMode(BilgeRelay_PIN, OUTPUT);
+
+  Serial.println("Instantiating servos");
+  GearServo = Servo(GearServo_PIN, DS3225_Servo_Freq, PWM_RESOLUTION_BITS, 0, 2, GearServo_REVERSE, GearServo_FORWARD);
+  ThrottleServo = Servo(ThrottleServo_PIN, DS3225_Servo_Freq, PWM_RESOLUTION_BITS, 0, 84000, ThrottleServo_MIN, ThrottleServo_MAX);
 
   Serial.println("Configuring WiFi");
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
+
+  // Change our MAC to the one we want
+  esp_err_t err = esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
+  if (err != ESP_OK) {
+    Serial.println("Failed to change MAC address!");
+  }
 
   uint8_t baseMac[6];
   esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
@@ -200,13 +200,38 @@ void setup()
   Serial.println("Done");
 }
 
+int gearStateToServoInput(FOREBOARD_GEAR_STATE gs) {
+  // This maps from the range received from the foreboard (-1, 0, 1) to values in the range
+  // which makes the servo behave appropriately.
+
+  switch (gs) {
+  case GEAR_STATE_REVERSE:
+    return 2;
+  case GEAR_STATE_NEUTRAL:
+    return 1;
+  case GEAR_STATE_FORWARD:
+    return 0;
+  default:
+    Serial.printf("Can't map invalid gear state: %d\n", gs);
+    return 1;
+  }
+}
+
+int throttleStateToServoInput(int throttle) {
+  // Need to invert the throttle signal so the throttle servo moves in the correct direction
+  if (throttle > THROTTLE_INPUT_MAX) {
+    throttle = THROTTLE_INPUT_MAX;
+  }
+  return THROTTLE_INPUT_MAX - throttle;
+}
+
 // ----------------------------
 //   MAIN
 // ----------------------------
 // This loop runs continuously
 void loop()
 {
-  if (cycle++ % 100 == 0) {
+  if (cycle++ % 500 == 0) {
     dumpState(&foreBoard);
     Serial.printf("Packets received in last period: %lld\n", pktsReceived);
     pktsReceived = 0;
@@ -232,20 +257,9 @@ void loop()
   {
     digitalWrite(BilgeRelay_PIN, LOW);
   }
-
-  switch (safeGearState)
-  {
-  case GEAR_STATE_REVERSE:
-    GearServo.write(GearServo_REVERSE);
-  case GEAR_STATE_NEUTRAL:
-    GearServo.write(GearServo_NEUTRAL);
-  case GEAR_STATE_FORWARD:
-    GearServo.write(GearServo_FORWARD);
-  default:
-    break;
-  }
-
-  ThrottleServo.write(map(foreBoard.throttle, 0, 10000, ThrottleServo_MIN, ThrottleServo_MAX));
-
+  
+  GearServo.write(gearStateToServoInput(safeGearState));
+  ThrottleServo.write(throttleStateToServoInput(foreBoard.throttle));
   delay(1);
 }
+ 
